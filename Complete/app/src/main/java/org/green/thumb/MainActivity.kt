3cd2d872daeb897804a013025352fb22f239ee24
Paul Lammertsma <plammer@google.com>
2022-06-20 15:06:59 +0200

    rudimentary adaptive layouts

--- Complete/app/src/main/java/org/green/thumb/MainActivity.kt
+++ Complete/app/src/main/java/org/green/thumb/MainActivity.kt
@@ -3,16 +3,20 @@ package org.green.thumb
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.core.view.WindowCompat
 import org.green.thumb.ui.theme.GreenThumbsTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             GreenThumbsTheme {
-                NavGraph()
+                val windowSize = calculateWindowSizeClass(this)
+                NavGraph(windowSize.widthSizeClass)
             }
         }
     }
--- Complete/app/src/main/java/org/green/thumb/welcome/PagerItem.kt
+++ Complete/app/src/main/java/org/green/thumb/welcome/PagerItem.kt
@@ -3,6 +3,8 @@ package org.green.thumb.welcome
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -13,6 +15,7 @@ internal fun PagerItem(page: Int) {
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     ) {
         when {
             page.isFirst() -> PageOne()
